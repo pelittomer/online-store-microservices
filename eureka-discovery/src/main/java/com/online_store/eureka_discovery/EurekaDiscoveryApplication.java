bf1704ad719d9eefed01e120a028dfd842ee43d7
pelittomer <pelittomer99@gmail.com>
2025-08-23 19:28:54 +0300

    feat: Add Eureka discovery server and client setup.

---
+++ eureka-discovery/src/main/java/com/online_store/eureka_discovery/EurekaDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.online_store.eureka_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryApplication.class, args);
+	}
+
+}
