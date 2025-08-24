38a0655fb0ed15db4f9f9a3a7abe92b4f18dd331
pelittomer <pelittomer99@gmail.com>
2025-08-24 23:56:47 +0300

    feat: Implement OpenFeign client for auth-service and upload-service communication.

---
+++ product-service/src/main/java/com/online_store/product_service/client/upload/UploadResponse.java
@@ -0,0 +1,5 @@
+package com.online_store.product_service.client.upload;
+
+public record UploadResponse(Long upload) {
+
+}
---
+++ product-service/src/main/java/com/online_store/product_service/client/user/UserResponse.java
@@ -0,0 +1,5 @@
+package com.online_store.product_service.client.user;
+
+public record UserResponse(Long id, String email, String role) {
+
+}
